876ec4353dcc12abdd1697c5412d781f83dc0152
MeesOverbeek <31857177+MeesOverbeek@users.noreply.github.com>
2019-01-25 10:53:31 +0100

    Update BIN_LEVEL.py

--- Afvalproject Hardware/BIN_LEVEL.py
+++ Afvalproject Hardware/BIN_LEVEL.py
@@ -6,42 +6,28 @@ from ECHO_SENSOR_2 import *
 import MySQLdb
 import datetime
 
-def executor():
-    afstand_1()
-    afstand_2()
-    return executor
-
-executor()
-
-
+#FUNCTION
+'''definieer twee functie die het gemiddelde
+van de beide sensoren berekenen'''
 
 def GemCalc():
-    try:
-        inhoud = open('Waarden.txt', 'r')
-        reader = inhoud.readlines()
-        counter = 0
-        for line in reader:
-           conv_int = int(line)
-           counter = counter + conv_int
-           Total = int(round(counter / 2))
-        return Total
-    except:
-       print("")
+    waarde1=420
+    waarde2=420
+    while waarde1 >100:
+        waarde1=afstand_1()[1]
+    while waarde2 >100:
+        waarde2=afstand_2()[1]
+    return (waarde1 + waarde2) / 2
+
 
 def CmCalc():
-    try:
-        inhoud = open('Centimeters.txt', 'r')
-        reader = inhoud.readlines()
-        counter = 0
-        for line in reader:
-           conv_int = int(line)
-           counter = counter + conv_int
-           PreTotal = int(round(counter / 2))
-           Total = 50 - PreTotal
-        return Total
- 
-    except:
-       print("")
+    waardeCm1 = 420
+    waardeCm2 = 420
+    while waardeCm1 > 50:
+        waardeCm1=round(afstand_1()[0])
+    while waardeCm2 > 50:
+        waardeCm2 = round(afstand_2()[0])
+    return 50 - (waardeCm1 + waardeCm2) / 2
 
 
 #SETUP
@@ -49,20 +35,20 @@ db=MySQLdb.connect(host="10.0.0.210",user="sensor",
                   passwd="buitenbankje123",db="vuilcontainerproject")
 cursor = db.cursor()
 
-#FUNCTIE
+#FUNCTION
 def INSERT_SQL(FK_vuilcontainerID, percentageDiepte, diepteAfvalCM, gewichtKG, datum):
     sql = "INSERT INTO vuilcontainerStatus(FK_vuilcontainerID, percentageDiepte, diepteAfvalCM, gewichtKG, datum) VALUES (%s, %s, %s, %s, %s)"
     cursor.execute(sql, (FK_vuilcontainerID, percentageDiepte, diepteAfvalCM, gewichtKG, datum))
 
-#VOORBEELD
-
-try:
-	INSERT_SQL("VC69", GemCalc(), CmCalc(), random.randint(1, 5), datetime.datetime.now())
-	db.commit()
-except:
-	db.rollback
+#INSERT
+while True:
+    
+    try:
+        INSERT_SQL("VC69", GemCalc(), CmCalc(), weight(), datetime.datetime.now())
+        db.commit()
+    except:
+        db.rollback
+    time.sleep(10)
 
 db.close()
 
-open('Waarden.txt', 'w').close
-open('Centimeters.txt', 'w').close
